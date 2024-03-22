fddc3387c9d8865f3c2df6e942b635c20bb9f303
shaikhasif69 <theshaikhasif03@gmail.com>
2024-03-22 05:35:18 +0530

    hello there sai

--- health_care_app/lib/main.dart
+++ health_care_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:health_care_app/pages/register.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'pages/signIn.dart';
 import 'test/app_screen_time.dart';
@@ -8,8 +10,29 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoginStatus();
+  }
+
+  Future<void> checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool loggedIn = prefs.getBool('isLoggedIn') ?? false;
+    setState(() {
+      isLoggedIn = loggedIn;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +42,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: UserDashBoard(),
+      // home: isLoggedIn ? UserDashBoard() : SignIn(),
+      home: SignIn(),
     );
   }
 }
-
